a1ccb7058f2a34760f61ecced56978198d7cd72b
Your Name <you@example.com>
2019-02-20 16:21:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+function domContentLoaded(){
+  document.addEventListener("DOMContentLoaded", function(event) {updateDOM()} )
+}
+
+ function updateDOM(){
+  document.getElementById("text").textContent = "This is really cool!"
+}
